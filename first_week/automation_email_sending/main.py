d9acb516f317973d96327aff80fe63f20b15ac44
muzeffer270 <t.muzeffer.1998@gmail.com>
2023-01-22 22:34:03 +0100

    i created logging file for logging our email

--- first_week/automation_email_sending/main.py
+++ first_week/automation_email_sending/main.py
@@ -1,10 +1,21 @@
-from email_sender import EmailSender
+from email_service import EmailService
 from files import FileService
+from logging_email import LoggingModule
 
 
-files = FileService
-email = EmailSender()
-email.send_email(files.get_files())
+logger = LoggingModule()
+
+try:
+    files = FileService
+    email = EmailService()
+    email.send_email(files.get_files())
+    logger.log_info('Emails were sent successfully')
+except Exception as e:
+    logger.log_error(f"ERROR: {str(e)}")
+
+
+
+
 
 
     
