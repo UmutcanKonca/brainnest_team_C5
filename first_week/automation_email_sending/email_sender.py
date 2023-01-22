87a0529533e82efa8aaae0bf48310c94700f097a
muzeffer270 <t.muzeffer.1998@gmail.com>
2023-01-22 20:33:35 +0100

    some changes done in main ,email_sender and function name was changed in files.py

--- first_week/automation_email_sending/email_sender.py
+++ first_week/automation_email_sending/email_sender.py
@@ -3,10 +3,14 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from datetime import date
 
+import requests
+
+response = requests.get(url='https://api.npoint.io/de5c3533349a7f588f4d')
+email_data = response.json()
 
 class EmailSender:
-    def __init__(self,email_data):
-        self.email_data = email_data
+  
+    def __init__(self):
         self.emails = [email for index, email in enumerate(email_data['emails'])] 
         self.subject = email_data['subject']
         self.content = email_data['content']
@@ -14,7 +18,6 @@ class EmailSender:
         self.sender_password = email_data['sender_email']['password']
         self.today = date.today().strftime("%d %B %Y")
         
-
     def send_email(self,files):
             with smtplib.SMTP(host='smtp.gmail.com',port=587) as connection:
                 subject = self.subject.replace('[DATE]',self.today)
@@ -27,8 +30,8 @@ class EmailSender:
                     message['To'] = email
                     message['Subject'] = subject
 
-                    message_content = f'{self.content.replace("[NAME]",self.email_data["emails"][email]["name"].title()).replace("[DATE]",self.today)}'
-                    message.attach(MIMEText(message_content))     
-                    message.attach(files)
+                    message_content = f'{self.content.replace("[NAME]",email_data["emails"][email]["name"].title()).replace("[DATE]",self.today)}'
+                    message.attach(MIMEText(message_content))
+                    for file in files:     
+                        message.attach(file)
                     connection.sendmail(from_addr=self.sender_email, to_addrs=email, msg=message.as_string())
-
--- first_week/automation_email_sending/main.py
+++ first_week/automation_email_sending/main.py
@@ -1,25 +1,10 @@
 from email_sender import EmailSender
-
-import requests
-from email.mime.base import MIMEBase
-from email import encoders
-import os
+from files import FileService
 
 
-path = "./files"
-files = os.listdir(path)
-new_file = None
-for file in files:
-    with open(os.path.join(path,file), 'rb') as attachment:
-        # read binary
-        our_file = MIMEBase('application',f'{file.split(".")[1]}', Name=file)
-        our_file.set_payload(attachment.read())
-        encoders.encode_base64(our_file)
-    new_file=our_file
+files = FileService
+email = EmailSender()
+email.send_email(files.get_files())
 
 
-if __name__ == "__main__":
-    response = requests.get(url='https://api.npoint.io/de5c3533349a7f588f4d')
-    email_data = response.json()
-    email = EmailSender(email_data)
-    email.send_email(new_file)
+    
